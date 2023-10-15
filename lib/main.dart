298f1322c7e7269beac45b1d2d563d407f882a0f
Sankaram <das95sankaram@gmail.com>
2023-10-15 17:11:01 +0530

    grid views

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-  List<String> names = ['Abc', 'DEF', 'ghi', 'JKL', 'mno', 'PQR'];
+  // List<String> names = ['Abc', 'DEF', 'ghi', 'JKL', 'mno', 'PQR'];
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +22,12 @@ class MyApp extends StatelessWidget {
           leading: Icon(Icons.menu),
           actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
         ),
-        body: ListView.builder(
-          itemCount: names.length,
-          itemBuilder: (context, index) => ListTile(
-            title: Text(
-              names[index],
-            ),
+        body: GridView.builder(
+          itemCount: 64,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+          itemBuilder: (context, index) => Container(
+            color: Colors.purpleAccent,
+            margin: EdgeInsets.all(10.0),
           ),
         ),
       ),
