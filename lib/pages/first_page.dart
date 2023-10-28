a604146bf45d621c2724373b74fa3d953fb19c5f
Sankaram <das95sankaram@gmail.com>
2023-10-28 16:50:54 +0530

    buttonMenuBar

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter_0to100/pages/profile_page.dart';
+import 'package:learn_flutter_0to100/pages/settings_page.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class FirstPage extends StatefulWidget {
+   FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+   //this keeps track of the current pgs to display
+   int _selectedIndex = 0;
+
+   //method to update the new selected index
+   void _navigateButtonBar(int index) {
+     setState(() {
+       _selectedIndex = index;
+     });
+   }
+
+  final List _pages = [
+    //Home_page
+    FirstPage(),
+
+    //settings_page
+    SttingsPage(),
+
+    //profile_page
+    ProfilePage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,41 +37,50 @@ class FirstPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('1\'st Page'),
       ),
-      drawer: Drawer(
-        backgroundColor: Colors.deepPurple[100],
-        child: Column(
-          children: [
-            DrawerHeader(
-                child: Icon(
-              Icons.favorite,
-              size: 40.0,
-              color: Colors.red,
-            ),),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Profile'),
-              onTap: (){
-                //pop the drawer(history)
-                Navigator.pop(context);
-
-                //Navigate the profile
-                Navigator.pushNamed(context, '/profilepage');
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('Settings'),
-              onTap: (){
-                //pop the drawer(history)
-                Navigator.pop(context);
-
-                //Navigate to settings page
-                Navigator.pushNamed(context, '/settingspage');
-              },
-            ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateButtonBar,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings),label: 'Settings'),
+        ],
       ),
+      // drawer: Drawer(
+      //   backgroundColor: Colors.deepPurple[100],
+      //   child: Column(
+      //     children: [
+      //       DrawerHeader(
+      //           child: Icon(
+      //         Icons.favorite,
+      //         size: 40.0,
+      //         color: Colors.red,
+      //       ),),
+      //       ListTile(
+      //         leading: Icon(Icons.person),
+      //         title: Text('Profile'),
+      //         onTap: (){
+      //           //pop the drawer(history)
+      //           Navigator.pop(context);
+      //
+      //           //Navigate the profile
+      //           Navigator.pushNamed(context, '/profilepage');
+      //         },
+      //       ),
+      //       ListTile(
+      //         leading: Icon(Icons.settings),
+      //         title: Text('Settings'),
+      //         onTap: (){
+      //           //pop the drawer(history)
+      //           Navigator.pop(context);
+      //
+      //           //Navigate to settings page
+      //           Navigator.pushNamed(context, '/settingspage');
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -7,6 +7,8 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Profile Page'),),
+      backgroundColor: Colors.red,
+      body: Center(child: Text('Profile')),
     );
   }
 }
